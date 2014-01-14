5f9b31ee50bca824123a093befa2758d4f28a4c5
Anya Hargil <anya.hargil@gmail.com>
2014-01-14 14:45:21 -0500

    Update Ships.java

--- Ships.java
+++ Ships.java
@@ -1,20 +1,20 @@
 public class Ships {
 
-  private int _ship;
-  
-  public Ship(String name) {
-    if(name.equals("aircraft carrier") {
-      _ship = "| | | | |";
-    }
-    else if(name.equals("battleship") {
-      _ship = "| | | |";
-    }
-    else if(name.equals("destroyer")) {
-      _ship = "| |";
-    }
-    else {
-      _ship = "| | |";
-    }
+  private String _ship;
+
+  public Ships(String name) {
+ 	  if(name.equals("aircraft carrier")) {
+ 	    _ship = "| | | | |";
+ 	  }
+ 	  else if(name.equals("battleship")) {
+ 	    _ship = "| | | |";
+ 	  }
+ 	  else if(name.equals("destroyer")) {
+ 	    _ship = "| |";
+ 	  }
+ 	  else {
+ 	    _ship = "| | |";
+ 	  }
   }
   
 }
