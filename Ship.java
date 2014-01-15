244c1cf0450deebfba4e09ae2638afd9806ef312
Anya Hargil <anya.hargil@gmail.com>
2014-01-15 14:46:36 -0500

    Update and rename Ships.java to Ship.java

---
+++ Ship.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Ship {
+
+  private int _lives;
+  private static int _size;
+  private ArrayList<String> coors = new ArrayList<String>();
+
+  public Ship(String name) {
+	  if(name.equals("aircraft carrier")) {
+ 	    _lives = 5;
+ 	    _size = 5;
+	  }
+	  else if(name.equals("battleship")) {
+ 	    _lives = 4;
+ 	    _size = 4;
+  	}
+	  else if(name.equals("destroyer")) {
+ 	   _lives = 2;
+ 	    _size = 2;
+	  }
+	  else {
+ 	   _lives = 3;
+ 	   _size = 3;
+  	}
+  }
+  
+  public boolean setShip(String orient, String letter, int num) {
+	
+	if(orient.equals("horizontal")) {
+	  if(num + _size > 9) {
+		  System.out.println("Invalid position. Ship does not fit on board.");
+		  return false;
+	   }
+	   else {
+		    for(int x = 0; x < _size; x++) {
+		      String coor = letter + num;
+		      coors.add(coor);
+		      num++;
+		  }
+		  return true;
+	    }
+	}
+	else {
+
+	}
+    }
+  
+}
