f8672ea17e001f6a3486b3f4ee0bf4fbecbf3f1e
andylstuy <andyl.stuy@gmail.com>
2014-01-15 14:44:41 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -26,6 +26,55 @@ public class Player {
   }
   
   public void play(){
+  	  /*      Creates the Battleship grid, though it does not have any spaces yet and 10 is represented by 0
+  	private static final String NUMBERS = " 1234567890";
+  	private static final String LETTERS = "ABCDEFGHIJ";
+      
+  	public static void print( String[][] a ) {
+		for (int r = 0; r < a.length; r++) {
+	    		for (int c = 0; c < a[r].length; c++)
+	    			System.out.print(a[r][c]);
+	    		System.out.println();
+  		}
+  	}
+
+  public static void grid(String[][] a) {
+	  for (int r = 0; r < a.length; r++) {
+	    for (int c = 0; c < a[r].length; c++) {
+		    if (r < 1)
+		      a[r][c] = NUMBERS.substring(c,c+1);
+		    if (r > 0 && c < 1)
+		      a[r][c] = LETTERS.substring(r-1,r);
+		    if (r > 0 && c > 0)
+		      a[r][c] = "~";
+	    }
+	  }
+  }
+
+  public static void main(String[] args) {
+	String[][] yourTargetGrid = new String[11][11];
+	grid(yourTargetGrid);
+	System.out.println("Your Target Grid:");
+	print(yourTargetGrid);
+	String[][] yourOceanGrid = new String[11][11];
+	grid(yourOceanGrid);
+	System.out.println("Your Ocean Grid:");
+	print(yourOceanGrid);
+
+	/*System.out.println();
+	System.out.println();
+
+	String[][] computarTargetGrid = new String[11][11];
+	grid(computarTargetGrid);
+	System.out.println("Computar's Target Grid:");
+	print(computarTargetGrid);
+	String[][] computarOceanGrid = new String[11][11];
+	grid(computarOceanGrid);
+	System.out.println("Computar's Ocean Grid:");
+	print(computarOceanGrid);*/
+	/*
+  }*/
+
   }
 
 }
