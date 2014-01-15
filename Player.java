6df27a08e5caa24dee629ac5d4d21062a6529397
Anya Hargil <anya.hargil@gmail.com>
2014-01-15 07:04:29 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,31 @@
-public interface Player {
+import cs1.Keyboard;
+
+public class Player {
   
-  public void setup();
-  public void play();
+  public void setup(){
+	  int numPlayers = 0;
+    
+	  System.out.println("Welcome to Battleship!");
+    
+	  while(numPlayers != 1 || numPlayers != 2) {
+      System.out.println("Would you like to play a one player or two player game?");
+	    System.out.print("Input 1 to play a one player game or 2 to play a two player game: ");
+	    numPlayers = Keyboard.readInt();
+      
+	    if(numPlayers == 1) {
+		    return setupComp();
+	    }
+	    else if(numPlayers == 2) {
+		    return setupHum();
+	    }
+	    else {
+	    	System.out.println("You did not enter valid input.");
+		    System.out.println("Please try again.");
+	    }
+	  }
+  }
+  
+  public void play(){
+  }
 
 }
