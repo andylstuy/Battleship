6ae8bb0cfa663c81ab83c49c8db6078dcf55c9d7
andylstuy <andyl.stuy@gmail.com>
2014-01-16 03:09:49 -0500

    Create NumericInput.java
    Demonstrates usage of the Scanner class - only here for reference

---
+++ NumericInput.java
@@ -0,0 +1,44 @@
+//**********************************************************************
+// Class NumericInput demonstrates reading numeric values using Scanner.
+//**********************************************************************
+
+import java.util.Scanner;
+import java.io.*;          // Access System.out
+
+public class NumericInput {
+
+    public static void main(String[] args) {
+
+	// Declarations
+	Scanner in = new Scanner(System.in);
+	int integer;
+	long longInteger;
+	float realNumber;
+	double doubleReal;
+	String string1;
+	String string2;
+
+	// Prompts
+	System.out.println("Enter an integer, a long integer, "
+			   + "a floating-point ");
+	System.out.println("number, another floating-point number, "
+			   + "and a string.");
+	System.out.println("Separate each with a blank or return.");    
+ 
+	// Read in values   
+	integer = in.nextInt();
+	longInteger = in.nextLong();
+	realNumber = in.nextFloat();
+	doubleReal = in.nextDouble();
+	string1 = in.nextLine();
+	System.out.println("Now enter another value.");
+	string2 = in.next();
+	System.out.println("Here is what you entered: ");
+	System.out.println(integer + " " + longInteger + " " + realNumber +
+			   " " + doubleReal + string1 + 
+			   " and " + string2);
+
+    }
+
+
+}
