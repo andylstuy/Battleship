74eccee3c95bbc2686562c06e2a7b69727be7426
Anya Hargil <anya.hargil@gmail.com>
2014-01-17 14:46:19 -0500

    Update Computar.java

--- Computar.java
+++ Computar.java
@@ -1,5 +1,34 @@
-public class Computar {
+import java.util.Scanner;
+import java.io.*;
+
+public class Computar extends Player() {
+
+  static String orient;
+  static String letter;
+  static int num;
+  
+  static Ship airCraft = new Ship("aircraft carrier");
+  static Ship battle = new Ship("battleship");
+  static Ship cruiser = new Ship("cruiser");
+  static Ship submarine = new Ship("submarine");
+  static Ship destroyer = new Ship("destroyer");
+
+  public static void setupComp(){
+
+    
+    airCraft.setShip(orient, letter, num);
 
 
+    battle.setShip(orient, letter, num);
+
+    
+    cruiser.setShip(orient, letter, num);
+
+
+    submarine.setShip(orient, letter, num);
+
+    
+    destroyer.setShip(orient, letter, num);
+  }
 
 }
