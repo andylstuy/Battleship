fc89213066b8eb749df1d91ce0d47b102e08a730
Anya Hargil <anya.hargil@gmail.com>
2014-01-17 12:30:13 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,6 +1,17 @@
 public class Human extends Player {
+  
+  Ship airCraft = new Ship("aircraft carrier");
+  Ship battle = new Ship("battleship");
+  Ship cruiser = new Ship("cruiser");
+  Ship submarine = new Ship("submarine");
+  Ship destroyer = new Ship("destroyer");
 
   public void setupHum(){
+    System.out.println("Please place your aircraft carrier. This ship is five units long.");
+    System.out.println("Choose the orientation(horizontal or vertical) and the top left coordinate of the ship.");
+    System.out.println("The coordinates range from A0 to J9");
+    System.out.println("Separate each entry by a space.");
+    System.out.print("Orientation, letter of coordinate, number of coordinate: ")
     
   }
 
