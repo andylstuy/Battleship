544cfaa3c736d337abe4f6eda6c14d8039d9f8bf
Anya Hargil <anya.hargil@gmail.com>
2014-01-17 06:20:49 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -9,67 +9,66 @@ public class Ship {
     private ArrayList<String> coors = new ArrayList<String>();
 
     public Ship(String name) {
-	if(name.equals("aircraft carrier")) {
-	    _name = name;
- 	    _lives = 5;
- 	    _size = 5;
-	}
-	else if(name.equals("battleship")) {
-	    _name = name;
- 	    _lives = 4;
- 	    _size = 4;
-	}
-	else if(name.equals("destroyer")) {
-	    _name = name;
- 	    _lives = 2;
- 	    _size = 2;
-	}
-	else {
-	    _name = name;
- 	    _lives = 3;
- 	    _size = 3;
-	}
+        if(name.equals("aircraft carrier")) {
+            _name = name;
+	    _lives = 5;
+	    _size = 5;
+        }
+        else if(name.equals("battleship")) {
+            _name = name;
+	    _lives = 4;
+	    _size = 4;
+        }
+        else if(name.equals("destroyer")) {
+            _name = name;
+	    _lives = 2;
+	    _size = 2;
+        }
+        else {
+            _name = name;
+	    _lives = 3;
+	    _size = 3;
+        }
     }
 
     public int numifyLetter(String letter) {
-	for(int x = 0; x < LETTERS.length(); x++) {
-	    if(LETTERS.substring(x, x+1).equals(letter)) {
-		return x;
-	    }
-	}
-	return -1;
+        for(int x = 0; x < LETTERS.length(); x++) {
+            if(LETTERS.substring(x, x+1).equals(letter)) {
+                return x;
+            }
+        }
+        return -1;
     }
   
     public boolean setShip(String orient, String letter, int num) {
-	
-	if(orient.equals("horizontal")) {
-	    if(num + _size > 9) {
-		System.out.println("Invalid position. Ship does not fit on board.");
-		return false;
-	    }
-	    else {
-		for(int x = 0; x < _size; x++) {
-		    String coor = letter + num;
-		    coors.add(coor);
-		    num++;
-		}
-		return true;
-	    }
-	}
-	else {
-	    if(numifyLetter(letter) + _size > 9) {
-		System.out.println("Invalid position. Ship does not fit on board.");
-		return false;
-	    }
-	    else {
-		for(int x = 0; x < _size; x++) {
-		    String coor = letter + num;
-		    coors.add(coor);
-		    letter = LETTERS.substring(numifyLetter(letter), numifyLetter(letter) + 1);
-		}
-		return true;
-	    }
-	}
+        
+        if(orient.equals("horizontal")) {
+            if(num + _size > 9) {
+                System.out.println("Invalid position. Ship does not fit on board.");
+                return false;
+            }
+            else {
+                for(int x = 0; x < _size; x++) {
+                    String coor = letter + num;
+                    coors.add(coor);
+                    num++;
+                }
+                return true;
+            }
+        }
+        else {
+            if(numifyLetter(letter) + _size > 9) {
+                System.out.println("Invalid position. Ship does not fit on board.");
+                return false;
+            }
+            else {
+                for(int x = 0; x < _size; x++) {
+                    String coor = letter + num;
+                    coors.add(coor);
+                    letter = LETTERS.substring(numifyLetter(letter) + 1, numifyLetter(letter) + 2);
+                }
+                return true;
+            }
+        }
     }
-  
 }
