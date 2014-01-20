0aa2610c549123f14a24e4e4b3ff61ad005119b0
Anya Hargil <anya.hargil@gmail.com>
2014-01-20 10:50:04 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -25,7 +25,7 @@ public class Human extends Player {
     
 	airCraft.setShip(orient, letter, num);
 
-	System.out.println("Please place your battleship. This ship is five units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
+	System.out.println("Please place your battleship. This ship is four units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
 	System.out.print("Orientation, letter of coordinate, number of coordinate: ");
     
 	orient = in.next();
@@ -34,7 +34,7 @@ public class Human extends Player {
     
 	battle.setShip(orient, letter, num);
 
-	System.out.println("Please place your cruiser. This ship is five units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
+	System.out.println("Please place your cruiser. This ship is three units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
 	System.out.print("Orientation, letter of coordinate, number of coordinate: ");
     
 	orient = in.next();
@@ -43,7 +43,7 @@ public class Human extends Player {
     
 	cruiser.setShip(orient, letter, num);
 
-	System.out.println("Please place your submarine. This ship is five units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
+	System.out.println("Please place your submarine. This ship is three units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
 	System.out.print("Orientation, letter of coordinate, number of coordinate: ");
     
 	orient = in.next();
@@ -52,7 +52,7 @@ public class Human extends Player {
     
 	submarine.setShip(orient, letter, num);
 
-	System.out.println("Please place your destroyer. This ship is five units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
+	System.out.println("Please place your destroyer. This ship is two units long. \nChoose the orientation (horizontal or vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9. \nSeparate each entry by a space.");
 	System.out.print("Orientation, letter of coordinate, number of coordinate: ");
     
 	orient = in.next();
