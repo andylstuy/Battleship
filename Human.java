8e1de33c1f9311b46ad5431ebf114f33777256be
andylstuy <andyl.stuy@gmail.com>
2014-01-23 22:59:22 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -9,6 +9,7 @@ public class Human extends Player {
     static String letter;
     static int num;
 
+
     public static void setupForOneHuman(){
 	System.out.println("\nFor each ship select the orientation(h for horizontal, v for vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9.\nSelect the letter(it must be uppercase) and the number and separate all entries with a space. \ni.e. h F 7");
 
@@ -144,16 +145,310 @@ public class Human extends Player {
 
     }
 
-    public static void play() {
-	/* {
-	    ;
-	    }*/
+
+
+    public static void setupForFirstHuman(){
+	System.out.println("\nFor each ship select the orientation(h for horizontal, v for vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9.\nSelect the letter(it must be uppercase) and the number and separate all entries with a space. \ni.e. h F 7");
+
 	
-	    System.out.println("It's your turn, Player. Make your move... \nWhere would you like to attack?");
-	    attackLetter = in.next();
-	    attackNum = in.nextInt();
-	    hit(computar1, computarOceanGrid, attackLetter, attackNum);
-	    	
+	//Aircraft Placement
+	boolean set = false;
+	while (set == false){
+	    System.out.println("\nPlease place your aircraft carrier. This ship is five units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = aircraft.setShip(orient, letter, num);
+	    
+	}
+	gridifyMe();
+	
+	//Battleship Placement
+	set = false;
+	while (set == false) {
+	    
+	    System.out.println("\nPlease place your battleship. This ship is four units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+	    
+	    set = battle.setShip(orient, letter, num);
+	    if (battle.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(battle);
+	    }
+	    
+	}
+	gridifyMe();
+	
+	//Cruiser Placement
+	set = false;
+	while (set == false) {
+
+	    System.out.println("\nPlease place your cruiser. This ship is three units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = cruiser.setShip(orient, letter, num);
+	    if (cruiser.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(cruiser);
+	    }
+	    if (cruiser.isOccupied(battle) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(cruiser);
+	    }
+
+	}
+	gridifyMe();
+	
+	//Submarine Placement
+	set = false;
+	while (set == false) {
+
+	    System.out.println("\nPlease place your submarine. This ship is three units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = submarine.setShip(orient, letter, num);
+	    if (submarine.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(submarine);
+	    }
+	    if (submarine.isOccupied(battle) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(submarine);
+	    }
+	    if (submarine.isOccupied(cruiser) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(submarine);
+	    }
+
+	}
+	gridifyMe();
+	
+	//Destroyer Placement
+	set = false;
+	while (set == false) {
+
+	    System.out.println("\nPlease place your destroyer. This ship is two units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = destroyer.setShip(orient, letter, num);
+	    if (destroyer.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+	    if (destroyer.isOccupied(battle) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+	    if (destroyer.isOccupied(cruiser) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+	    if (destroyer.isOccupied(submarine) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+ 
+	}
+	gridifyMe();
+
+    }
+
+
+
+
+    public static void setupForSecondHuman(){
+	System.out.println("\nFor each ship select the orientation(h for horizontal, v for vertical) and the top left coordinate of the ship. \nThe coordinates range from A0 to J9.\nSelect the letter(it must be uppercase) and the number and separate all entries with a space. \ni.e. h F 7");
+
+	
+	//Aircraft Placement
+	boolean set = false;
+	while (set == false){
+	    System.out.println("\nPlease place your aircraft carrier. This ship is five units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = aircraft.setShipCPU(orient, letter, num);
+	    
+	}
+	gridifyOther();
+	
+	//Battleship Placement
+	set = false;
+	while (set == false) {
+	    
+	    System.out.println("\nPlease place your battleship. This ship is four units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+	    
+	    set = battle.setShipCPU(orient, letter, num);
+	    if (battle.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(battle);
+	    }
+	    
+	}
+	gridifyOther();
+	
+	//Cruiser Placement
+	set = false;
+	while (set == false) {
+
+	    System.out.println("\nPlease place your cruiser. This ship is three units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = cruiser.setShipCPU(orient, letter, num);
+	    if (cruiser.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(cruiser);
+	    }
+	    if (cruiser.isOccupied(battle) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(cruiser);
+	    }
+
+	}
+	gridifyOther();
+	
+	//Submarine Placement
+	set = false;
+	while (set == false) {
+
+	    System.out.println("\nPlease place your submarine. This ship is three units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = submarine.setShipCPU(orient, letter, num);
+	    if (submarine.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(submarine);
+	    }
+	    if (submarine.isOccupied(battle) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(submarine);
+	    }
+	    if (submarine.isOccupied(cruiser) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(submarine);
+	    }
+
+	}
+	gridifyOther();
+	
+	//Destroyer Placement
+	set = false;
+	while (set == false) {
+
+	    System.out.println("\nPlease place your destroyer. This ship is two units long.");
+	    System.out.print("Orientation, letter of coordinate, number of coordinate: ");
+    
+	    orient = in.next();
+	    letter = in.next();
+	    num = in.nextInt();
+    
+	    set = destroyer.setShipCPU(orient, letter, num);
+	    if (destroyer.isOccupied(aircraft) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+	    if (destroyer.isOccupied(battle) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+	    if (destroyer.isOccupied(cruiser) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+	    if (destroyer.isOccupied(submarine) == false) {
+		System.out.println("Ship already here! \nTry again.");
+		set = false;
+		resetCoors(destroyer);
+	    }
+ 
+	}
+	gridifyOther();
+
+    }
+
+
+
+
+    public static void play() {
+	
+	System.out.println("It's your turn, Player. Make your move... \nWhere would you like to attack?");
+	attackLetter = in.next();
+	attackNum = in.nextInt();
+	hit(computar1, computarOceanGrid, attackLetter, attackNum);
+	
+    }
+
+
+    public static void play1() {
+	
+	System.out.println("It's your turn, Player 1. Make your move... \nWhere would you like to attack?");
+	attackLetter = in.next();
+	attackNum = in.nextInt();
+	hit1(human2, otherOceanGrid, attackLetter, attackNum);
+	
+    	
+    }
+
+    public static void play2() {
+	
+	System.out.println("It's your turn, Player 2. Make your move... \nWhere would you like to attack?");
+	attackLetter = in.next();
+	attackNum = in.nextInt();
+	hit2(human1, yourOceanGrid, attackLetter, attackNum);
 
     }
 
